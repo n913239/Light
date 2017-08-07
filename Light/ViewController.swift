85bf07e4d43d1db70f0f19c835d947822ae964e8
n913239 <n913239@gmail.com>
2017-08-07 11:05:29 +0800

    lightRainbow

--- Light/ViewController.swift
+++ Light/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
@@ -20,15 +21,51 @@ class ViewController: UIViewController {
         } else {
             view.backgroundColor = UIColor.black
         }
-    }    
+    }
+    
+    // 產生 0 ~ 6 的亂數，可以用 nextInt()取得新的亂數
+    let randomNumber = GKRandomDistribution(lowestValue: 0, highestValue: 6)
+    // 儲放目前顏色的Index，範圍從 0 ~ 6
+    var colorIndex = 0
+    // 儲存顏色與RGB Values的Dictionary
+    var rainbowColors = ["Red":[255,0,0],
+                         "Orange":[255, 127, 0],
+                         "Yellow":[255, 255, 0],
+                         "Green":[0, 255, 0],
+                         "Blue":[0, 0, 255],
+                         "Indigo":[75, 0, 130],
+                         "Violet":[148,0,211]]
+    // 設定七色彩虹的背景顏色
+    func setRainbowColor() {
+        // 透過colorIndex索引，來設定取得顏色的索引字串 color
+        var color = "Red"
+        switch colorIndex {
+        case 0: color = "Red"
+        case 1: color = "Orange"
+        case 2: color = "Yellow"
+        case 3: color = "Green"
+        case 4: color = "Blue"
+        case 5: color = "Indigo"
+        case 6: color = "Violet"
+        default: color = "Red"
+        }
+        view.backgroundColor = UIColor(red: CGFloat(rainbowColors[color]![0]),
+                                       green: CGFloat(rainbowColors[color]![1]),
+                                       blue: CGFloat(rainbowColors[color]![2]), alpha: 1)
+        // 將顏色索引+1，如果大於6，就重0開始
+        colorIndex += 1
+        colorIndex = (colorIndex > 6) ? 0 : colorIndex
+    }
     
     // 透過Tap Gesture 來連結Action
     @IBAction func changeBackgroundColor(_ sender: Any) {
-        updateLightStatus()
+        setRainbowColor()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        colorIndex =  randomNumber.nextInt()
+        setRainbowColor()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
